e59d51f6047730000a520837cf487582497dfedc
Lan Dang <danghoanglan271@gmail.com>
2022-12-26 14:06:00 +0700

    Update EEGNet, Stacked BiLSTM, CNN Models

--- Experiment/EEG_Encoder/EEGNet_original.py
+++ Experiment/EEG_Encoder/EEGNet_original.py
@@ -20,47 +20,42 @@ class classifier_EEGNet(nn.Module):
         D = 2
         first_kernel = temporal//2
         first_padding = first_kernel//2
-        self.network = nn.Sequential( #input: (batch, channel, 96, 512)
-            nn.ZeroPad2d((first_padding, first_padding-1, 0, 0)), #(1, 1, 96, 767)
+        self.network = nn.Sequential( #input size: (batch, channel, 96, 512)
+            nn.ZeroPad2d((first_padding, first_padding-1, 0, 0)),
             nn.Conv2d(in_channels = 1,
                       out_channels = F1,
-                      kernel_size = (1, first_kernel)), #(1, 8, 96, 512)
+                      kernel_size = (1, first_kernel)),
             nn.BatchNorm2d(F1),
             nn.Conv2d(in_channels = F1,
-                      out_channels = D*F1,
+                      out_channels = F1,
                       kernel_size = (spatial, 1),
-                      groups = F1), #(1, 16, 1, 512)
-            # nn.Conv2d(in_channels = F1,
-            #           out_channels = D*F1,
-            #           kernel_size = 1), 
+                      groups = F1),
+            nn.Conv2d(in_channels = F1,
+                      out_channels = D*F1,
+                      kernel_size = 1),
             nn.BatchNorm2d(D*F1),
             nn.ELU(),
-            nn.AvgPool2d(kernel_size = (1, 4)), #(1, 16, 1, 128)
+            nn.AvgPool2d(kernel_size = (1, 4)),
             nn.Dropout(),
-            nn.ZeroPad2d((8, 7, 0, 0)), #(1, 16, 1, 143)
+            nn.ZeroPad2d((8, 7, 0, 0)),
             nn.Conv2d(in_channels = D*F1,
                       out_channels = D*F1,
                       kernel_size = (1, 16),
-                      groups = F1), #(1, 16, 1, 128)
+                      groups = F1),
             nn.Conv2d(in_channels = D*F1,
                       out_channels = F2,
-                      kernel_size = 1), #(1, 16, 1, 128)
+                      kernel_size = 1),
             nn.BatchNorm2d(F2),
             nn.ELU(),
-            nn.AvgPool2d(kernel_size = (1, 8)), #(1, 16, 1, 16)
+            nn.AvgPool2d(kernel_size = (1, 8)),
             nn.Dropout())
-        self.fc = nn.Linear(F2*(temporal//32), 40) #input size=16x(512/8)=256; output size=40
+        self.fc = nn.Linear(F2*(temporal//32), 40)
 
     def forward(self, x):
-        #input size of x: (batch, 96, 512)
-        print("Input size: ",x.size())
-        x = x.unsqueeze(0).permute(1, 0, 2, 3) #(batch, 1, 96, 512)
-        print("After permute: ",x.size())
-        x = self.network(x) #(batch, 16, 1, 16)
-        print("After network: ",x.size())
-        x = x.view(x.size()[0], -1) #(batch, 16x16)
-        print("After view: ",x.size())
-        return self.fc(x) 
+        x = x.unsqueeze(0).permute(1, 0, 3, 2)
+        x = self.network(x)
+        x = x.view(x.size()[0], -1)
+        return self.fc(x)
 
     def cuda(self, gpuIndex):
         self.network = self.network.cuda(gpuIndex)
