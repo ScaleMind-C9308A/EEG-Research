03e442bdbbd391a0d3384bda5a940d93127280ef
Lan Dang <danghoanglan271@gmail.com>
2023-05-06 10:56:59 +0700

    Update

---
+++ Experiment/TripleNet/dataset.py
@@ -0,0 +1,134 @@
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import BatchSampler
+from torchvision.transforms import Compose
+import torch
+import numpy as np
+from PIL import Image
+
+class EEGDataset(Dataset):
+    """
+    Train: For each sample (anchor) randomly chooses a positive and negative samples
+    Test: Creates fixed triplets for testing
+    """
+    def __init__(self, opt, eeg_signals_path):
+        """
+        Args:
+            opt: {
+                subject:,
+                time_low:,
+                time_high:,
+                model_type:
+            }
+        """
+        self.opt = opt
+        # Load EEG signals
+        loaded = torch.load(eeg_signals_path)
+        if opt.subject!=0:
+            self.data = [loaded['dataset'][i] for i in range(len(loaded['dataset']) ) if loaded['dataset'][i]['subject']==opt.subject]
+        else:
+            self.data=loaded['dataset']        
+        self.labels = loaded["labels"]
+        self.images = loaded["images"]
+
+        # self.mnist_dataset = mnist_dataset
+        # self.train = self.mnist_dataset.train
+        # self.transform = self.mnist_dataset.transform
+
+        if self.train:
+            self.train_labels = self.labels
+            self.train_data = self.data
+            self.labels_set = set(self.train_labels.numpy())
+            self.label_to_indices = {label: np.where(self.train_labels.numpy() == label)[0]
+                                     for label in self.labels_set}
+
+        else:
+            self.test_labels = self.labels
+            self.test_data = self.data
+            # generate fixed triplets for testing
+            self.labels_set = set(self.test_labels.numpy())
+            self.label_to_indices = {label: np.where(self.test_labels.numpy() == label)[0]
+                                     for label in self.labels_set}
+
+            random_state = np.random.RandomState(29)
+
+            triplets = [[i,
+                         random_state.choice(self.label_to_indices[self.test_labels[i].item()]),
+                         random_state.choice(self.label_to_indices[
+                                                 np.random.choice(
+                                                     list(self.labels_set - set([self.test_labels[i].item()]))
+                                                 )
+                                             ])
+                         ]
+                        for i in range(len(self.test_data))]
+            self.test_triplets = triplets
+
+    def __getitem__(self, index):
+        """
+        Call Data[i] will return a tuple that contain the triplets
+        ds = EEGDataset()
+        ds[i] will return by what we define __getitem__ magic methods
+        """
+        if self.train:
+            img1, label1 = self.train_data[index], self.train_labels[index].item()
+            positive_index = index
+            while positive_index == index:
+                positive_index = np.random.choice(self.label_to_indices[label1])
+            negative_label = np.random.choice(list(self.labels_set - set([label1])))
+            negative_index = np.random.choice(self.label_to_indices[negative_label])
+            img2 = self.train_data[positive_index]
+            img3 = self.train_data[negative_index]
+        else:
+            img1 = self.test_data[self.test_triplets[index][0]]
+            img2 = self.test_data[self.test_triplets[index][1]]
+            img3 = self.test_data[self.test_triplets[index][2]]
+
+        img1 = Image.fromarray(img1.numpy(), mode='L')
+        img2 = Image.fromarray(img2.numpy(), mode='L')
+        img3 = Image.fromarray(img3.numpy(), mode='L')
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+        return (img1, img2, img3), []
+
+    def __len__(self):
+        return len(self.mnist_dataset)
+    
+class BalancedBatchSampler(BatchSampler):
+    """
+    BatchSampler - from a MNIST-like dataset, samples n_classes and within these classes samples n_samples.
+    Returns batches of size n_classes * n_samples
+    """
+
+    def __init__(self, labels, n_classes, n_samples):
+        self.labels = labels
+        self.labels_set = list(set(self.labels.numpy()))
+        self.label_to_indices = {label: np.where(self.labels.numpy() == label)[0]
+                                 for label in self.labels_set}
+        for l in self.labels_set:
+            np.random.shuffle(self.label_to_indices[l])
+        self.used_label_indices_count = {label: 0 for label in self.labels_set}
+        self.count = 0
+        self.n_classes = n_classes
+        self.n_samples = n_samples
+        self.n_dataset = len(self.labels) # num of samples in dataset
+        self.batch_size = self.n_samples * self.n_classes
+
+    def __iter__(self):
+        self.count = 0
+        while self.count + self.batch_size < self.n_dataset:
+            classes = np.random.choice(self.labels_set, self.n_classes, replace=False)
+            indices = []
+            for class_ in classes:
+                indices.extend(self.label_to_indices[class_][
+                               self.used_label_indices_count[class_]:self.used_label_indices_count[
+                                                                         class_] + self.n_samples])
+                self.used_label_indices_count[class_] += self.n_samples
+                if self.used_label_indices_count[class_] + self.n_samples > len(self.label_to_indices[class_]):
+                    np.random.shuffle(self.label_to_indices[class_])
+                    self.used_label_indices_count[class_] = 0
+            yield indices
+            self.count += self.n_classes * self.n_samples
+
+    def __len__(self):
+        return self.n_dataset // self.batch_size
---
+++ Experiment/TripleNet/run_online_triplet.py
@@ -0,0 +1,32 @@
+from datasets import BalancedBatchSampler
+# Set up the network and training parameters
+# from networks import EmbeddingNet
+import torch
+from losses import OnlineTripletLoss
+from utils import AllTripletSelector,HardestNegativeTripletSelector, RandomNegativeTripletSelector, SemihardNegativeTripletSelector # Strategies for selecting triplets within a minibatch
+from metrics import AverageNonzeroTripletsMetric
+import torch.optim as optim
+import torch.optim.lr_scheduler as lr_scheduler
+from trainer import net_trainer
+
+# We'll create mini batches by sampling labels that will be present in the mini batch and number of examples from each class
+train_batch_sampler = BalancedBatchSampler(train_dataset.train_labels, n_classes=10, n_samples=25)
+test_batch_sampler = BalancedBatchSampler(test_dataset.test_labels, n_classes=10, n_samples=25)
+
+kwargs = {'num_workers': 1, 'pin_memory': True} if cuda else {}
+online_train_loader = torch.utils.data.DataLoader(train_dataset, batch_sampler=train_batch_sampler, **kwargs)
+online_test_loader = torch.utils.data.DataLoader(test_dataset, batch_sampler=test_batch_sampler, **kwargs)
+
+margin = 1.
+embedding_net = EmbeddingNet()
+model = embedding_net
+if cuda:
+    model.cuda()
+loss_fn = OnlineTripletLoss(margin, RandomNegativeTripletSelector(margin))
+lr = 1e-3
+optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-4)
+scheduler = lr_scheduler.StepLR(optimizer, 8, gamma=0.1, last_epoch=-1)
+n_epochs = 20
+log_interval = 150
+
+net_trainer(online_train_loader, online_test_loader, model, loss_fn, optimizer, scheduler, n_epochs, cuda, log_interval, metrics=[AverageNonzeroTripletsMetric()])
