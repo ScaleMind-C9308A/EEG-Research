7b8ec017c54b4e0396cf2596efc4f61b988ca7bd
Huyen <ngoanhhuyen@gmail.com>
2023-06-13 22:49:13 +0700

    u

--- Experiment/Generative_Models/ACGAN/trainer_GAN.py
+++ Experiment/Generative_Models/ACGAN/trainer_GAN.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
-from loguru import logger #??
+from loguru import logger 
 import matplotlib.pyplot as plt
 import os
 
