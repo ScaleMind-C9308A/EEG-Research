2e79069211407e9782b4fa8e54cddf1116c10586
Huyen <ngoanhhuyen@gmail.com>
2023-08-22 19:22:02 +0700

    u

--- Experiment/EEGClassification/Encoder/LSTM_Dropout.py
+++ Experiment/EEGClassification/Encoder/LSTM_Dropout.py
@@ -36,9 +36,14 @@ class classifier_LSTM_Dropout(nn.Module):
         # Change order of axis from (n, 96, 512) to (n, 512, 96)
         x = torch.permute(x, (0, 2, 1))
         batch_size = x.size(0)
-        lstm_init = (torch.zeros(self.lstm_layers, batch_size, self.lstm_size).to(self.device),
-                     torch.zeros(self.lstm_layers, batch_size, self.lstm_size).to(self.device))
+        lstm_init1 = (torch.zeros(self.lstm_layers, batch_size, self.lstm_size_1).to(self.device),
+                     torch.zeros(self.lstm_layers, batch_size, self.lstm_size_1).to(self.device))
+        lstm_init2 = (torch.zeros(self.lstm_layers, batch_size, self.lstm_size_2).to(self.device),
+                     torch.zeros(self.lstm_layers, batch_size, self.lstm_size_2).to(self.device))
         # lstm_init = (Variable(lstm_init[0]), Variable(lstm_init[1]))
-        x = self.lstm(x, lstm_init)[0][:, -1, :]
+        x = self.lstm1(x, lstm_init1)[0][:, -1, :]
+        x = self.dropout1(x)
+        x = self.lstm2(x, lstm_init2)[0][:, -1, :]
+        x = self.dropout2(x)
         x = self.output1(x)
         return x
