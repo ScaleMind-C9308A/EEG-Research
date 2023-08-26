027373ac64a093f0b4872cdab28595aa8423973e
Lan Dang <danghoanglan271@gmail.com>
2023-08-26 19:09:50 +0700

    Update

--- Experiment/EEGClassification/Encoder/EEGWaveNet.py
+++ Experiment/EEGClassification/Encoder/EEGWaveNet.py
@@ -4,7 +4,7 @@ from torch import nn
 
 class EEGWaveNet_Encoder(nn.Module):
     def __init__(self, n_chans,n_classes=40):
-        super(Net, self).__init__()
+        super(EEGWaveNet_Encoder, self).__init__()
 
         self.temp_conv1 = nn.Conv1d(n_chans, n_chans, kernel_size=2, stride=2 ,groups=n_chans)
         self.temp_conv2 = nn.Conv1d(n_chans, n_chans, kernel_size=2, stride=2 ,groups=n_chans)
