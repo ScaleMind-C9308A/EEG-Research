b3073100e5909af6d8d4c7c252f59aa58eaf919c
Lan Dang <danghoanglan271@gmail.com>
2023-09-09 18:59:48 +0700

    Update

--- Experiment/EEGClassification/utils/eeg_dataset_splits.py
+++ Experiment/EEGClassification/utils/eeg_dataset_splits.py
@@ -7,10 +7,12 @@ import torch
 import numpy as np
 
 splits_all_path = '/media/mountHDD1/LanxHuyen/CVPR2017/block_splits_by_image_all.pth' 
-eeg_path = '/media/mountHDD1/LanxHuyen/CVPR2017/eeg_55_95_std.pth'
+splits_removed_33 = '/media/mountHDD1/LanxHuyen/CVPR2017/splits_by_image_removed_33.pth' 
+eeg_path = '/media/mountHDD1/LanxHuyen/CVPR2017/eeg_5_95_std.pth'
 output_path_dir = '/media/mountHDD1/LanxHuyen/CVPR2017'
 
 splits_all = torch.load(splits_all_path)
+splits_removed33 = torch.load(splits_removed_33)
 eeg_loaded = torch.load(eeg_path)
 dataset, labels, images = [eeg_loaded[k] for k in eeg_loaded.keys()]
 
@@ -26,17 +28,24 @@ def remove_label_from_splits(dataset, splits_all, label):
     torch.save(splits, os.path.join(output_path_dir, "splits_by_image_removed_33.pth"))
 
 def group_splits_by_subject(dataset, splits):
-    splits = splits['splits'][0]
+    # splits = splits['splits'][0]
     result_splits = {}
     for key, split in splits.items():
         images_idx = np.array([dataset[idx]['image'] for idx in split]) # => len(images_idx) = len(split)
-        grouped_images_idx = np.array([np.where(images_idx == image)[0] for image in np.unique(images_idx)]) #(split_len, 6)
+        grouped_images_idx = []
+        for image_idx in np.unique(images_idx):
+            arr = [split[i] for i in np.where(images_idx == image_idx)[0]] # (6, )
+            grouped_images_idx.append(arr)
+
+        # grouped_images_idx = np.array(grouped_images_idx) #(split_len, 6)
+        
         result_splits[key] = grouped_images_idx
-    torch.save(result_splits, os.path.join(output_path_dir, "splits_by_subject_1.pth"))
+    torch.save(result_splits, os.path.join(output_path_dir, "splits_by_subject_2.pth"))
 
 if __name__ == "__main__":
     # remove_label_from_splits(dataset, splits_all, 33)
-    group_splits_by_subject(dataset, splits_all)
+    # group_splits_by_subject(dataset, splits_all['splits'][0])
+    group_splits_by_subject(dataset, splits_removed33)
 
 
 
