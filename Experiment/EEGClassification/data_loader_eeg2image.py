115238ecbe51b19b35f0686b9e1d6d11afaf8da8
Lan Dang <danghoanglan271@gmail.com>
2023-09-26 21:54:06 +0700

    Update

--- Experiment/EEGClassification/data_loader_eeg2image.py
+++ Experiment/EEGClassification/data_loader_eeg2image.py
@@ -79,7 +79,7 @@ class EEG2Image_Augment_Dataset(Dataset):
         normalized_data = (eeg - eeg.min()) / (eeg.max() - eeg.min())
         grayscale_images = (normalized_data * 255).to(torch.uint8)
         grayscale_images = grayscale_images.unsqueeze(0).unsqueeze(0) # (1, 1, h, w)
-        eeg_heatmap = F.interpolate(grayscale_images, size=(512, 440), mode='bilinear', align_corners=True)
+        eeg_heatmap = F.interpolate(grayscale_images, size=(512, 440), mode='bicubic', align_corners=True)
         eeg_heatmap = eeg_heatmap.squeeze(0).squeeze(0)
         # Can try this to avoid UserWarning
         # eeg_heatmap = eeg_heatmap.clone().detach().requires_grad_(True) 
