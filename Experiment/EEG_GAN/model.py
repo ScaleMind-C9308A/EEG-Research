1ffa4b31e1902ccf5e23b4555b45a3e12ea63418
Lan Dang <danghoanglan271@gmail.com>
2023-10-20 10:45:58 +0700

    Update

--- Experiment/EEG_GAN/model.py
+++ Experiment/EEG_GAN/model.py
@@ -17,11 +17,11 @@ def load_model(model_name, weight_path=None, args=None):
     model_name: wgan_1_channel | wgan_multi_channel | cc_wgan
     """
     if model_name == "wgan_1_channel":
-        generator, discriminator = WGAN_GP_multichannel.GAN_Generator(), WGAN_GP_multichannel.GAN_Discriminator()
+        generator, discriminator = WGAN_GP_multichannel.EEGGenerator(), WGAN_GP_multichannel.EEGDiscriminator()
     elif model_name == "wgan_multi_channel":
-        generator, discriminator = WGAN_GP_multichannel.GAN_Generator(), WGAN_GP_multichannel.GAN_Discriminator()
+        generator, discriminator = WGAN_GP_multichannel.EEGGenerator(), WGAN_GP_multichannel.EEGDiscriminator()
     elif model_name == "cc_wgan":
-        generator, discriminator = WGAN_GP_multichannel.GAN_Generator(), WGAN_GP_multichannel.GAN_Discriminator()
+        generator, discriminator = WGAN_GP_multichannel.EEGGenerator(), WGAN_GP_multichannel.EEGDiscriminator()
     else:
         raise ValueError("model_name must be 'wgan_1_channel' | 'wgan_multi_channel' | 'cc_wgan'")        
     return generator, discriminator
