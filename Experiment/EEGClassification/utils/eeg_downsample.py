b2219a28bc3492c1246cf92b4755a30b2fec8365
Lan Dang <danghoanglan271@gmail.com>
2023-10-20 22:24:28 +0700

    Update

---
+++ Experiment/EEGClassification/utils/eeg_downsample.py
@@ -0,0 +1,31 @@
+from scipy import signal
+import os
+import random
+import torch
+import numpy as np
+import torch.nn.functional as F
+
+splits_all_path = '/media/mountHDD1/LanxHuyen/CVPR2017/block_splits_by_image_all.pth' 
+eeg_path = '/media/mountHDD1/LanxHuyen/CVPR2017/eeg_55_95_std.pth'
+output_path_dir = '/media/mountHDD1/LanxHuyen/CVPR2017'
+output_filename = 'eeg_55_95_std_downsampled_128Hz.pth'
+
+splits_all = torch.load(splits_all_path)
+eeg_loaded = torch.load(eeg_path)
+eeg_dataset, labels, images = [eeg_loaded[k] for k in eeg_loaded.keys()]
+
+def eeg_downsample(eeg_data, time_low=20, time_high=460):
+    """
+    Downsample EEG data from 440Hz to 128Hz.
+    """
+    eeg_data = eeg_data[:, time_low:time_high] # (128, 440) => cut eeg signal to 440 time steps
+    
+    # Resample from 440Hz to 128Hz
+    eeg_data = signal.resample_poly(eeg_data, up=128, down=440, axis=1, padtype='line') # (128, 128)
+    return eeg_data
+
+for sample in eeg_dataset:
+    sample['eeg'] = eeg_downsample(sample['eeg'])
+eeg_loaded['dataset'] = eeg_dataset
+torch.save(eeg_loaded, os.path.join(output_path_dir, output_filename))
+
