9cfb456253b0878ea175ea5bb06f0edb262d0d1c
Lan Dang <danghoanglan271@gmail.com>
2023-10-21 15:35:58 +0700

    Update

--- Experiment/EEGClassification/utils/eeg_downsample.py
+++ Experiment/EEGClassification/utils/eeg_downsample.py
@@ -8,27 +8,35 @@ import torch.nn.functional as F
 splits_all_path = '/media/mountHDD1/LanxHuyen/CVPR2017/block_splits_by_image_all.pth' 
 eeg_path = '/media/mountHDD1/LanxHuyen/CVPR2017/eeg_55_95_std.pth'
 output_path_dir = '/media/mountHDD1/LanxHuyen/CVPR2017'
-output_filename = 'eeg_55_95_std_downsampled_128Hz.pth'
+output_filename = 'eeg_55_95_std_downsampled_128Hz_resample.pth'
 
 splits_all = torch.load(splits_all_path)
 eeg_loaded = torch.load(eeg_path)
 eeg_dataset, labels, images = [eeg_loaded[k] for k in eeg_loaded.keys()]
 
-def eeg_downsample(eeg_data, time_low=20, time_high=460):
+def eeg_downsample(eeg_data, time_low=20, time_high=460, method='resample_poly'):
     """
     Downsample EEG data from 440Hz to 128Hz.
+    Args
+        eeg_data(np.array): EEG data (128, 440)
+        time_low(float): Lowest time value of eeg segment
+        time_high(float): highest time value of eeg segment
+        method(str): Downsample method (resample_poly | resample)
     """
     eeg_data = eeg_data[:, time_low:time_high] # (128, 440) => cut eeg signal to 440 time steps
     eeg_data = np.array(eeg_data) # (128, 440)
     # Resample from 440Hz to 128Hz
-    eeg_data = signal.resample_poly(eeg_data, up=128, down=440, axis=1, padtype='line') # (128, 128)
+    if method == 'resample':
+        eeg_data = signal.resample(eeg_data, num=128, axis=1) # (128, 128)
+    elif method == 'resample_poly':
+        eeg_data = signal.resample_poly(eeg_data, up=128, down=440, axis=1, padtype='line') # (128, 128)
     # Convert to torch tensor
     eeg_data = torch.tensor(eeg_data, dtype=torch.float32) # (128, 128)
     return eeg_data
 
 print(f"Before downsample: Size of an EEG sample (tensor): {eeg_dataset[0]['eeg'].size()}")
 for sample in eeg_dataset:
-    sample['eeg'] = eeg_downsample(sample['eeg'])
+    sample['eeg'] = eeg_downsample(sample['eeg'], method='resample')
 eeg_loaded['dataset'] = eeg_dataset
 print(f"After downsample: Size of an EEG sample (tensor): {eeg_dataset[0]['eeg'].size()}")
 torch.save(eeg_loaded, os.path.join(output_path_dir, output_filename))
